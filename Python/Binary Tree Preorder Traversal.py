3d0b178f03dd934a3ccd7868a5d7444b51b8980b
William <william@office.mac>
2018-04-03 08:53:46 +0800

    sync

---
+++ Python/Binary Tree Preorder Traversal.py
@@ -0,0 +1,43 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if root:
+            return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
+        else:
+            return []
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        visitlist = []
+        self.visit(root,visitlist)
+        return visitlist
+        
+    def visit(self, root,visitlist):
+        if root:
+            visitlist.append(root.val)
+        
+            self.visit(root.left, visitlist)
+        
+            self.visit(root.right,visitlist)
+        
